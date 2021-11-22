5cedafdbd00368cd581adbed3b30a0e4aebf017a
user <user@DESKTOP-7PO26L5>
2021-11-22 22:41:39 +0900

    문제풀이

--- java_practice_problem/src/re_solve_the_problem/ArrayEx17_문제.java
+++ java_practice_problem/src/re_solve_the_problem/ArrayEx17_문제.java
@@ -1,5 +1,9 @@
 package re_solve_the_problem;
 
+import java.util.Scanner;
+
+//풀이시간 : 2021-11-22 22:23 ~ 22:41
+
 /* 
  * # 틱택토
  * 
@@ -24,7 +28,85 @@ public class ArrayEx17_문제 {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		
 		int[] game = new int[9];
+		
+		int turn = 1;
+		int p1Win = 0;
+		int p2Win = 0;
+		
+		// 게임
+		while (true) {
+			
+			// 프론트
+			System.out.println("= 틱택토 =");
+			for (int i=0; i<game.length; i++) {
+				if 		(game[i] == 0) System.out.print("[ ]");
+				else if (game[i] == 1) System.out.print("[O]");
+				else if (game[i] == 2) System.out.print("[X]");
+				
+				if (i%3 == 2) System.out.println();
+				
+			}
+			
+			// 턴
+			if (turn % 2 == 1) {		
+				System.out.println("\n[시스템] Player1 차례입니다.");
+				System.out.print("[시스템] 인덱스를 입력하세요.");			
+			} else {
+				System.out.println("\n[시스템] Player2 차례입니다.");
+				System.out.print("[시스템] 인덱스를 입력하세요.");
+			}
+			
+			int idx = sc.nextInt();
+			if (idx < 0 || idx > 8) {
+				System.out.println("0~8까지의 숫자만 입력이 가능합니다.\n");
+				continue;
+			} else {
+				if (game[idx] == 0 && turn % 2 == 1) {
+					game[idx] = 1;
+					turn++;
+				} else if (game[idx] == 0 && turn % 2 == 0) {
+					game[idx] = 2;
+					turn++;
+				} else {
+					System.out.println("이미 선택 된 자리입니다.\n");
+				}
+			}
+			
+			// 승패계산
+			// 가로
+			if (game[0] == 1 && game[1] == 1 && game[2] == 1 || game[3] == 1 && game[4] == 1 && game[5] == 1 || game[6] == 1 && game[7] == 1 && game[8] == 1) {
+				p1Win = 1;
+			} else if (game[0] == 2 && game[1] == 2 && game[2] == 2 || game[3] == 2 && game[4] == 2 && game[5] == 2 || game[6] == 2 && game[7] == 2 && game[8] == 2) {
+				p2Win = 1;
+			}
+			
+			// 세로
+			if (game[0] == 1 && game[3] == 1 && game[6] == 1 || game[1] == 1 && game[4] == 1 && game[7] == 1 || game[2] == 1 && game[5] == 1 && game[8] == 1) {
+				p1Win = 1;
+			} else if (game[0] == 2 && game[3] == 2 && game[6] == 2 || game[1] == 2 && game[4] == 2 && game[7] == 2 || game[2] == 2 && game[5] == 2 && game[8] == 2) {
+				p2Win = 1;
+			}
+			
+			// 대각
+			if (game[0] == 1 && game[4] == 1 && game[8] == 1 || game[2] == 1 && game[4] == 1 && game[6] == 1) {
+				p1Win = 1;
+			} else if (game[0] == 2 && game[4] == 2 && game[8] == 2 || game[2] == 2 && game[4] == 2 && game[6] == 2) {
+				p2Win = 1;
+			}
+			
+			// 승패처리
+			if (p1Win == 1) {
+				System.out.println("플레이어1의 승리");
+				break;
+			} else if (p2Win == 1) {
+				System.out.println("플레이어2의 승리");
+				break;
+			}
+			
+		}
 		
 	}
 		
