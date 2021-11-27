53eb62f341e92d7ad17052d6934f77646326666b
user <user@DESKTOP-7PO26L5>
2021-11-27 11:57:35 +0900

    2021-11-27 문제풀이

--- java_practice_problem/src/re_solve_the_problem/ArrayEx23_문제.java
+++ java_practice_problem/src/re_solve_the_problem/ArrayEx23_문제.java
@@ -1,6 +1,9 @@
 package re_solve_the_problem;
 
+import java.util.Arrays;
 import java.util.Scanner;
+
+//풀이시간 : 2021-11-27 10:35 ~ 10:51
 
 /*
  * # ATM[3단계]
@@ -28,17 +31,91 @@ public class ArrayEx23_문제 {
 		
 		while (isRun) {
 			
-			System.out.println("1.가입");
+			System.out.println("accs : " + Arrays.toString(accs));
+			System.out.println("pws  : " + Arrays.toString(pws));
+			
+			System.out.println("\n1.가입");
 			System.out.println("2.탈퇴");
 			System.out.println("3.종료");
 			System.out.print("메뉴 선택 : ");
 			selectMenu = scan.nextInt();
 			
-			if		(selectMenu == 1) {}
-			else if (selectMenu == 2) {}
-			else if (selectMenu == 3) {}
+			if		(selectMenu < 1 || selectMenu > 3) {
+				continue;
+			}
+			else if	(selectMenu == 1) {
+				if (accsCnt == 5) {
+					System.out.println("더 이상 추가할 수 없습니다.");
+				} else {
+					System.out.println("ID를 입력하세요");
+					int myId = scan.nextInt();
+					
+					int cnt = 0;
+					for (int i=0; i<accsCnt; i++) {
+						if (myId == accs[i]) {
+							cnt = 1;
+							break;
+						}
+					}
+					if (cnt == 0) {
+						
+						accs[accsCnt] = myId;
+						
+						System.out.println("PW를 입력하세요");
+						pws[accsCnt] = scan.nextInt();
+						
+						accsCnt++;
+					} else {
+						System.out.println("ID가 중복됩니다.");
+					}
+				}
+			}
+			else if (selectMenu == 2) {
+				if (accsCnt == 0) {
+					System.out.println("삭제할 계정이 없습니다.");
+				} else if (accsCnt == 1) {
+					accs[0] = 0;
+					pws[0] = 0;
+					accsCnt --;
+				} else {
+					System.out.println("삭제할 아이디 입력");
+					int myId = scan.nextInt();
+					
+					int idx = -1;
+					for (int i=0; i<accsCnt; i++) {
+						if (myId == accs[i]) {
+							idx = i;
+							break;
+						}
+					}
+					
+					if (idx == -1) {
+						System.out.println("중복되는 ID가 없습니다.");
+					} else if (idx == accsCnt-1){
+						accs[idx] = 0;
+						pws[idx] = 0;
+						accsCnt--;
+					} else {
+						
+						for (int i=idx; i<accsCnt-1; i++) {
+							accs[i] = accs[i+1];
+							pws[i] = pws[i+1];
+							accs[i+1] = 0; 
+							pws[i+1] = 0; 
+						}
+						accsCnt --;
+						System.out.println("삭제 완료.");
+					}
+					
+				}
+			}
+			else if (selectMenu == 3) {
+				break;
+			}
 			
 		}
+		
+		System.out.println("\n프로그램을 종료합니다.");
 
 	}
 }
--- java_practice_problem/src/re_solve_the_problem/ArrayEx24_테스트문제1_1.java
+++ java_practice_problem/src/re_solve_the_problem/ArrayEx24_테스트문제1_1.java
@@ -3,13 +3,13 @@ package re_solve_the_problem;
 import java.util.Arrays;
 import java.util.Scanner;
 
-// 2021-10-02 12:30 ~ 12:50
+//풀이시간 : 2021-11-27 11:43 ~ 11:48
 
 public class ArrayEx24_테스트문제1_1 {
 
 	public static void main(String[] args) {
 		
-		Scanner scan = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		
 		int[] a = { 10, 4, 5, 3, 1 };
 		
@@ -18,30 +18,35 @@ public class ArrayEx24_테스트문제1_1 {
 		for (int i=0; i<a.length; i++) {
 			add += a[i];
 		}
-		System.out.println("정답 1) " + add);
-		System.out.println();
+		System.out.println("정답 1) " + add + "\n");
+		
 		
 		// 문제 2) 값을 입력하면 인덱스 출력
 		// 예) 5 ==> 2  ,  1 ==> 4
-		System.out.println("값 입력");
-		int num = scan.nextInt();
+		System.out.print("값 입력 : ");
+		int num = sc.nextInt();
 		
-		for(int i=0; i<a.length; i++) {
-			if (a[i] == num) {
-				System.out.println(i);
+		int idx = -1;
+		for (int i=0; i<a.length; i++) {
+			if (num == a[i]) {
+				idx = i;
+				break;
 			}
 		}
-		System.out.println();
+		if (idx == -1) {
+			System.out.println("올바른 값을 입력하시오\n");
+		} else {
+			System.out.println("정답 2) " + idx + "\n");
+		}
 		
 		
 		// 문제 3)  인덱스를 입력하면 값 출력
 		// 예) 2 ==> 5  , 4 ==> 1
-		System.out.println("값 입력");
-		int idx = scan.nextInt();
+		System.out.print("인덱스 입력 : ");
+		idx = sc.nextInt();
+		System.out.println("정답 3) " + a[idx] + "\n");
 		
-		System.out.println(a[idx]);
-		System.out.println();
-
+		
 		// 문제 4) 위 배열중 가장 큰 값 출력 
 		// 예) 10
 		int max = 0;
@@ -50,45 +55,44 @@ public class ArrayEx24_테스트문제1_1 {
 				max = a[i];
 			}
 		}
-		System.out.println("최대값 : " + max);
-		System.out.println();
+		System.out.println("정답 4) " + max + "\n");
 		
 
+		
 		// 문제 5) 위 배열중 홀수의 개수 출력 
 		// 예) 홀수의 개수 : 3
-		int cnt = 0;
+		num = 0;
 		for (int i=0; i<a.length; i++) {
 			if (a[i] % 2 == 1) {
-				cnt++;
+				num++;
 			}
 		}
-		System.out.println("홀수 갯수 : " + cnt);
-			
+		System.out.println("정답 5) " + num + "\n");
+		
+		
 		// 문제 6) a의 값중 홀수만 b에 저장 (저장위치는 a와 b의 동일한 위치에 저장)
 		// 예) b => {0, 0, 5, 3, 1}
-		int[] b = {0, 0, 0, 0, 0};
+		int[] b = new int[5];
 		for (int i=0; i<a.length; i++) {
 			if (a[i] % 2 == 1) {
 				b[i] = a[i];
 			}
 		}
-		System.out.println(Arrays.toString(b));
-		System.out.println();
+		System.out.println("정답 6) " + Arrays.toString(b)+ "\n");
+		
 		
 		
 		// 문제 7) 다음 리스트를 이용해서 a의 값중 홀수만 c에 저장(저장위치는 앞에서부터  저장 , 3번 문제와 다름)
 		// 예) b => {5, 3, 1, 0, 0}
-		int[] c = {0, 0, 0, 0, 0};
-		int arrNum = 0;
+		int[] c = new int[5];
+		int cnt = 0;
 		for (int i=0; i<a.length; i++) {
 			if (a[i] % 2 == 1) {
-				c[arrNum] = a[i];
-				arrNum ++;
+				c[cnt] = a[i];
+				cnt++;
 			}
 		}
-		System.out.println(Arrays.toString(c));
-
-		scan.close();
+		System.out.println("정답 6) " + Arrays.toString(c)+ "\n");
 		
 	}
 
